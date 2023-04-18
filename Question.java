f1ada50b248b3c590310bec8d80800f2ed9616a4
theoconnetable <56427602+theoconnetable@users.noreply.github.com>
2023-04-18 09:19:38 +0200

    question

--- Question.java
+++ Question.java
@@ -1,23 +1,38 @@
+import java.util.ArrayList; // import the ArrayList class
+
+
 public class Question{
     private String id;
     private String question;
     private Boolean fin;
-    private Question[] nexts;
+    private ArrayList<Question> nexts;
 
     //INITIALISATION VIDE (PAS SUR QU'ELLE SOIT UTILISEE)
     Question(){
         this.id = "";
         this.question = "";
         this.fin = true;
-        this.nexts = null;
+        this.nexts = new ArrayList<Question>();
     }
     /////////////////////////////////////////////////////
 
-    Question(String id,String question,Boolean fin,Question[] nexts){
+    Question(String id,String question,Boolean fin,ArrayList<Question> nexts){
         this.id = id;
         this.question = question;
         this.fin = fin;
         this.nexts = nexts;
+    }
+    
+    public boolean estFin(){
+        if(this.fin == false) {
+        	return false;
+        }
+        return true;
+    }
+    
+    public void addNext(Question quest){
+    		this.nexts.add(quest);
+    		this.fin = false;
     }
 
 }
