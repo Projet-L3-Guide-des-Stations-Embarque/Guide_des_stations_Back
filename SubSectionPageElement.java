79f995c3c879f791405fa47dc38930336e331a8c
MaxBuster <maxbuster380@gmail.com>
2023-04-18 10:56:02 +0200

    PageElements

---
+++ SubSectionPageElement.java
@@ -0,0 +1,22 @@
+package bgfhnvbfgv;
+
+import java.util.ArrayList;
+
+public class SubSectionPageElement implements PageElement {
+	// INSTANCE ATTRIBUTES
+	private String titre;
+	private ArrayList<PageElement> elements;
+		
+	// CONSTRUCTORS
+	public SubSectionPageElement(String titre) {
+		this.titre = titre;
+		elements = new ArrayList<PageElement>();
+	}
+		
+	// PUBLIC INSTANCE METHODS
+	public String getTitre() {return titre;}
+	public void setTitre(String nouv) {titre = nouv;}
+	
+	public void addElement(PageElement nouv) {elements.add(nouv);}
+	public void removeElement(PageElement cible) {elements.remove(cible);}
+}
