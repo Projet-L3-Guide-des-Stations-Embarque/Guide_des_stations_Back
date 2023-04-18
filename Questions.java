458b333656bbe06296c84e5e15c33344d4e6bfa3
theoconnetable <56427602+theoconnetable@users.noreply.github.com>
2023-04-18 09:49:42 +0200

    Questions to Json V1

--- Questions.java
+++ Questions.java
@@ -1,3 +1,39 @@
-public class Questions {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Questions {
+	private ArrayList<Question> liste;
+	
+	Questions(){
+		this.liste = new ArrayList<Question>();
+	}
+	Questions( ArrayList<Question> liste){
+		this.liste = liste;
+	}
+	
+	public void ecrit(File filename) throws IOException{
+		String str = "Hello";
+	    BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+	    writer.write(str);
+	    writer.close();
+	}
+	
+	public void confirmation(){
+		try {
+			File myObj = new File("question.json");
+		    if (myObj.createNewFile()) {
+		    	System.out.println("File created: " + myObj.getName());
+		    	ecrit(myObj);
+		    } else {
+		        System.out.println("File already exists.");
+		    }
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+		    e.printStackTrace();
+		}
+	}
+	
 }
---
+++ mainQuestion.java
@@ -0,0 +1,8 @@
+public class mainQuestion {
+
+	public static void main(String[] args) {
+		Questions test = new Questions();
+		test.confirmation();
+	}
+
+}
