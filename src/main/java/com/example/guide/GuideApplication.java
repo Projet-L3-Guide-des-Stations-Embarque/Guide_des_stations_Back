bc39d617eee778ce20657c7adc3c9adbeb9ac611
Khmarigou <luca.plc@outlook.fr>
2023-05-09 10:13:51 +0200

    Add init project

---
+++ src/main/java/com/example/guide/GuideApplication.java
@@ -0,0 +1,21 @@
+package com.example.guide;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class GuideApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(GuideApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
