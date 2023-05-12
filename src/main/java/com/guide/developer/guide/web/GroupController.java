53b7b6216439958bfef20bc7165734eba51493d2
Khmarigou <luca.plc@outlook.fr>
2023-05-12 11:21:42 +0200

    Gestion du nom du guide lors de l'upload

--- src/main/java/com/guide/developer/guide/web/GroupController.java
+++ src/main/java/com/guide/developer/guide/web/GroupController.java
@@ -1,13 +1,18 @@
 package com.guide.developer.guide.web;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.guide.developer.guide.model.Group;
 import com.guide.developer.guide.model.GroupRepository;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 import org.apache.tomcat.util.json.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -38,14 +43,33 @@ class GroupController {
     }
 
     @PostMapping("/upload")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, @RequestParam("dir") String dir, @Param("name") String name) {
         try {
             String filename = StringUtils.cleanPath(file.getOriginalFilename());
-            Path directory = Paths.get("./uploadedFiles");
+            log.info("Fichier mis en ligne: " + filename + " dans le dossier: " + dir + " avec le nom: " + name);
+            Path directory = Paths.get("./uploadedFiles/" + dir);
+            if (!Files.exists(directory)) {
+                Files.createDirectories(directory);
+                JSONArray guides = new JSONArray();
+                JSONObject guide = new JSONObject();
+                guide.put("nom", name);
+                guide.put("version", "1");
+                guide.put("lienQuestions", "");
+                guide.put("lienPages", "");
+                guide.put("lienGE", "");
+                guide.put("lienLexique", "");
+                guide.put("lienSecteursAltitudes", "");
+                guide.put("lienSecteursZones", "");
+                guides.add(guide);
+
+                Path filepath = directory.resolve("statutGuide.json");
+                Files.write(filepath, guides.toString().getBytes());
+            }
             Path filepath = directory.resolve(filename);
             Files.copy(file.getInputStream(), filepath, StandardCopyOption.REPLACE_EXISTING);
             return "Fichier mis en ligne avec succès!";
         } catch (IOException ex) {
+            log.info("Erreur lors de la mise en ligne du fichier: " + ex);
             return "Erreur lors de la mise en ligne";
         }
     }
@@ -90,8 +114,10 @@ class GroupController {
                     try {
                         String guideStatusContent = new String(Files.readAllBytes(guideStatusFile.toPath()));
                         JSONParser parser = new JSONParser(guideStatusContent);
-                        Map<String, Object> guideStatus = parser.parseObject();
-                        folderInfo.put("nom", guideStatus.get("nom").toString());
+                        List<Map<String, String>> guides = (List<Map<String, String>>) parser.parse();
+                        Map<String, String> guide = guides.get(0);
+                        String nom = guide.get("nom");
+                        folderInfo.put("nom", nom);
                         folderInfo.put("url", "/" + file.getName());
                     } catch (Exception e) {
                         e.printStackTrace();
